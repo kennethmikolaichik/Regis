65a78c381e3c09657e4b22b39907f3cf53269de9
kennethmikolaichik <kennethmikolaichik@gmail.com>
2023-04-03 09:55:13 -0500

    Update Full_Pan.py

--- Code Bits/Awaken/Full_Pan.py
+++ Code Bits/Awaken/Full_Pan.py
@@ -1,34 +1,35 @@
-""""Full_Pan"""
-
+"""Full_Pan"""
+#- - - -LIBRARIES- - - -#
 import pigpio
 import time
-#- - - - - - - - - - - -#
+#- -DEFINE PARAMETERS- -#
 position = 1500
-x = 0
-#- - - - - - - - - - - -#
 pi = pigpio.pi()
-
+#- - -MOTOR SETUP- - - -#
 pi.set_mode(22, pigpio.OUTPUT)
 pi.set_PWM_frequency(22, 50)
-
-pi.set_servo_pulsewidth(22, position) #sets pan to center
-
-while x < 100:
-    while position < 2250:    
-        position = position -10
-        print(position)
-        time.sleep(.1)
-        x = x+1
-        if position <= 750:
-            break
-    while position >= 750:
-        position = position +10
-        print(position)
-        time.sleep(.1)
-        x = x+1
-        if position >= 2250:
-            break
-    break
-print("completed Sucessfully")
+pi.set_servo_pulsewidth(22, position) #sets pan to center(1500)
+#- - - - - - - - - - - -#
+while position <= 2250:    
+    position = position -10
+    degree = ((position-1500)/(1000))*90
+    print(f"ServoSignal:{position}, Degree:{degree:.1f}°", end="\r")
+    time.sleep(.1)
+    if position <= 750:
+        break
+while position >= 750:
+    position = position +10
+    degree = ((position-1500)/(1000))*90
+    print(f"ServoSignal:{position}, Degree:{degree:.1f}°", end="\r")
+    time.sleep(.1)
+    if position >= 2250:
+        break
+while position <= 2250:    
+    position = position -10
+    degree = ((position-1500)/(1000))*90
+    print(f"ServoSignal:{position}, Degree:{degree:.1f}°", end="\r")
+    time.sleep(.1)
+    if position <= 1500:
+        break
+print("Completed Sucessfully")
 quit()
-
