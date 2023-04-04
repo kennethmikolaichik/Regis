dbfe280f937a6a1c3546ea1464e2ca376ce29d8b
kennethmikolaichik <kennethmikolaichik@gmail.com>
2023-04-04 17:56:48 -0500

    update

--- Code Bits/Awaken/Full_Pan.py
+++ Code Bits/Awaken/Full_Pan.py
@@ -13,23 +13,26 @@ pi.set_servo_pulsewidth(22, position) #sets pan to center(1500)
 while position <= 2250:    
     position = position -10
     degree = ((position-1500)/(1000))*90
-    print(f"ServoSignal:{position}, Degree:{degree:.1f}°", end="\r")
+    pi.set_servo_pulsewidth(22, position)
+    print(f"ServoSignal:{position}, Degree:{degree:.1f}° ", end="\r")
     time.sleep(.1)
     if position <= 750:
         break
 while position >= 750:
     position = position +10
     degree = ((position-1500)/(1000))*90
-    print(f"ServoSignal:{position}, Degree:{degree:.1f}°", end="\r")
+    pi.set_servo_pulsewidth(22, position)
+    print(f"ServoSignal:{position}, Degree:{degree:.1f}° ", end="\r")
     time.sleep(.1)
     if position >= 2250:
         break
 while position <= 2250:    
     position = position -10
     degree = ((position-1500)/(1000))*90
-    print(f"ServoSignal:{position}, Degree:{degree:.1f}°", end="\r")
+    pi.set_servo_pulsewidth(22, position)
+    print(f"ServoSignal:{position}, Degree:{degree:.1f}° ", end="\r")
     time.sleep(.1)
     if position <= 1500:
         break
-print("Completed Sucessfully")
+print("Full_Pan - completed sucessfully")
 quit()
---
+++ Code Bits/Awaken/Leg1_Tarsus_Test.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  4 17:03:45 2023
+
+@author: kenne
+"""
+
+
+import pigpio
+import time
+
+pi = pigpio.pi()
+# Set the default PWM frequency for all the servos to 50Hz
+DEFAULT_FREQ = 50
+T=5
+C=7
+F=6
+servos = [C, F, T]
+for pin in servos:
+    pi.set_mode(pin, pigpio.OUTPUT)    
+    pi.set_PWM_frequency(pin, DEFAULT_FREQ)
+    
+#Tarsus
+LGT_max = 2450
+LGT_min = 1150
+#Femur
+LGF_max = 2050
+LGF_min = 500
+#Coxa
+LGC_min = 1500 #do not know value
+LGC_max = 1500 #do not know value
+
+#- - - - - - - - - - - -#
+C_position = 1500
+F_position = 1500
+T_position = 1500
+while T_position <= LGT_max:    
+    T_position = T_position -10
+    degree = ((T_position-1500)/(1000))*90
+    pi.set_servo_pulsewidth(T, T_position)
+    print(f"Coxa ServoSignal:{T_position}, Degree:{degree:.1f}° ", end="\r")
+    time.sleep(.1)
+    if T_position <= LGT_min:
+        break
+while T_position >= LGT_min:
+    T_position = T_position +10
+    degree = ((T_position-1500)/(1000))*90
+    pi.set_servo_pulsewidth(T, T_position)
+    print(f"Coxa ServoSignal:{T_position}, Degree:{degree:.1f}° ", end="\r")
+    time.sleep(.1)
+    if T_position >= LGT_max:
+        break    
+    
+    
+    
+    
+print("Leg1_Tarsus_Test - completed sucessfully")
+quit()
+
