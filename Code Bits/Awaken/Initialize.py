1db3423aa1e0956735b5ba1afa83c5a3ed3187a4
kennethmikolaichik <kennethmikolaichik@gmail.com>
2023-07-09 23:46:52 -0500

    Update

--- Code Bits/Awaken/Initialize.py
+++ Code Bits/Awaken/Initialize.py
@@ -34,6 +34,28 @@ def Pan_Update():
 
     time.sleep(Speed) #Speed Controller
 
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+def Tilt_Update():
+
+    # Limiting Program: keeps motors from attempting to move past physical stops
+    # Replaces improper value with limit value
+    '''
+    if Desired_Pan >= P_max
+        Desired_Pan = P_max
+    if Desired_Pan <= P_min
+        Desired_Pan = P_min
+    '''
+    #if less than desired angle
+    if Tilt_Angle < Desired_Tilt:
+        Tilt_Angle +=0.1 #increase angle towards desired angle
+    #if greater than desired angle
+    if Tilt_Angle > Desired_Tilt:
+        Tilt_Angle -=0.1 #dencrease angle towards desired angle   
+    
+    PWM_Signal = ((1000 * Tilt_Angle) / 90) + 1500
+    pi.set_servo_pulsewidth(Pan, PWM_Signal)
+
+    time.sleep(Speed) #Speed Controller
 
 
 #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
@@ -914,13 +936,19 @@ while True:
                     # Move camera based on arrow keys
                     if event.key == pygame.K_LEFT:
                         camera_x -= CAMERA_SPEED
+                        Desired_Pan -=1
                         
                     elif event.key == pygame.K_RIGHT:
                         camera_x += CAMERA_SPEED
+                        Desired_Pan +=1
+
                     elif event.key == pygame.K_UP:
                         camera_y -= CAMERA_SPEED
+                        Desired_Tilt -=1
+
                     elif event.key == pygame.K_DOWN:
                         camera_y += CAMERA_SPEED
+                        Desired_Tilt +=1
 
             # Clear the screen
             screen.fill((0, 0, 0))
@@ -933,11 +961,10 @@ while True:
             
             # Update Servo Signal
             Pan_Update()
+            Tilt_Update()
 
         # Quit the program
         pygame.quit()
-
-
         Main_Pgm_Answer = 0
         break     
     #--------------------------------------------------------------------------
